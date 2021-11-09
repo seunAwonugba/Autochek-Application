724015d2dfcec0c09b4f8af40e293281cf5c89cc
seunawonugba <seun.awonugba@decagon.dev>
2021-11-09 19:01:14 +0100

    implement api interface

--- app/src/main/java/com/example/autochekapplication/api/ApiServiceInterface.kt
+++ app/src/main/java/com/example/autochekapplication/api/ApiServiceInterface.kt
@@ -1,5 +1,16 @@
 package com.example.autochekapplication.api
 
+import com.example.autochekapplication.constants.Constants.MAKE_END_POINT
+import com.example.autochekapplication.dataclass.MakeDataClass
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiServiceInterface {
 
+    @GET(MAKE_END_POINT)
+    suspend fun getMakeList(
+        @Query("popular")
+        popular : String = "true"
+    ) : Response<MakeDataClass>
 }
