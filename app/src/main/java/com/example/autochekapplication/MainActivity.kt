c120d080e45b00c4e6a712d95cb507e5b6981c4b
seunawonugba <seun.awonugba@decagon.dev>
2021-11-09 17:16:35 +0100

    linked bottom nav with nav controller

--- app/src/main/java/com/example/autochekapplication/MainActivity.kt
+++ app/src/main/java/com/example/autochekapplication/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.autochekapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.autochekapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var bottomNav : BottomNavigationView
+    private lateinit var navHostFragment : FragmentContainerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        bottomNav = findViewById(R.id.bottomNavId)
+        navHostFragment = findViewById(R.id.fragmentContainerView)
 
         //set up bottom app bar
         binding.bottomNavId.background = null
         binding.bottomNavId.menu.getItem(2).isEnabled = false
+
+        //set up bottom nav with fragments
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNav.setupWithNavController(navController)
     }
 }
